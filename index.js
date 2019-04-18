7d03fdce74efb55fc088f474884adf0ad639b82b
Alisa Filatova <alicefill@bk.ru>
2019-04-18 17:23:26 +0300

    updated styles

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const images = document.querySelectorAll('.item > img');
 const inventory = document.querySelector('.inventory');
+const openCaseBtn = document.querySelector('.open-case');
 
-inventory.addEventListener('click', () => {
+openCaseBtn.addEventListener('click', () => {
   if (inventory.classList.contains('shine')) {
     inventory.classList.remove('shine');
   }
@@ -9,12 +10,12 @@ inventory.addEventListener('click', () => {
   images.forEach((image) => {
     const imageY = image.getBoundingClientRect().top;
     const imageX = image.getBoundingClientRect().left;
-    const speed = 1000;
+    const speed = 1200;
     const curveDelay = 300;
     const position = 'fixed';
     const imageClone = image.cloneNode();
-    image.classList.add('fly');
 
+    image.classList.add('fly');
     imageClone.classList.add('flyingItem');
     imageClone.style.position = position;
     imageClone.style.top = `${imageY}px`;
@@ -24,21 +25,19 @@ inventory.addEventListener('click', () => {
     setTimeout(() => {
       image.classList.remove('fly');
       imageClone.style.transition = `all ${speed / 1000}s ease, top ${(speed + curveDelay) / 1000}s ease, left ${speed / 1000}s ease, transform ${speed / 1000}s ease ${(speed - 10) / 1000}s`;
-
       image.parentNode.insertBefore(imageClone, image.nextSibling);
 
       imageClone.style.top = `${inventory.offsetTop + inventory.offsetHeight - 50}px`;
       imageClone.style.left = `${inventory.offsetLeft + inventory.offsetWidth - 100}px`;
       imageClone.style.width = '63px';
-      imageClone.style.transform = 'scale(0.3)';
-      imageClone.style.opacity = '0';
+      imageClone.style.transform = 'scale(0.2)';
+      imageClone.style.opacity = '0.1';
       imageClone.classList.remove('flyingItem');
-
     }, 800);
 
     setTimeout(() => {
       inventory.classList.add('shine');
-    }, 1300);
+    }, 1400);
 
     setTimeout(() => {
       imageClone.remove();
